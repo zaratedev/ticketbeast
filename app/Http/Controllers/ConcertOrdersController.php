5c30318efd245e92f9b43c538525f24ed8a5e36d
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2019-07-12 14:14:51 -0500

    Adding Tickets to Orders

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -15,13 +15,20 @@ class ConcertOrdersController extends Controller
         $this->paymentGateway = $paymentGateway;
     }
     
-    public function store(Request $request, Concert $concert)
+    public function store(Request $request, $id)
     {
+        $concert = Concert::find($id);
         $ticketQuantity = $request->get('ticket_quantity');
         $amount = $ticketQuantity * $concert->ticket_price;
         $key = $request->get('payment_token');
-
         $this->paymentGateway->charge($amount, $key);
+
+        $order = $concert->orders()->create(['email' => $request->get('email')]);
+
+        foreach (range(1, $ticketQuantity) as $i) {
+            $order->tickets()->create([]);
+        }
+        
         return response()->json([], 201);
     }
 }
